298bfdef729505363883c91ae063114f16f7d014
Federico Melis <fedemelis1@gmail.com>
2023-09-30 17:11:02 +0200

    upd

--- excel-processer/main.py
+++ excel-processer/main.py
@@ -1,9 +1,12 @@
 import os
 import random
 import urllib.request
+from datetime import datetime
+
 import pandas as pd
 import json
 import bibtexparser
+import requests
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 from github import Github
@@ -52,9 +55,10 @@ def readexcelfile(file_name):
 
     print("Dati convertiti e salvati in formato BibTeX.")
 
-def web_file_downloader(url, file_name):
+def web_file_downloader(id, file_name):
+    link = f"https://drive.google.com/uc?export=download&id={id}"
     try:
-        urllib.request.urlretrieve(url, file_name)
+        urllib.request.urlretrieve(link, os.path.join("edata", file_name))
     except Exception as e:
         print(e)
         return False
@@ -68,11 +72,8 @@ def gituploader(github_username, git_token):
     g = Github(git_token)
 
     #g = Github("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")
-
-    #building repo name
     repo_name = github_username + ".github.io"
 
-    #getting the repo
     repo = g.get_user().get_repo(repo_name)
 
     # Get the current directory path
@@ -93,15 +94,78 @@ def gituploader(github_username, git_token):
         sha = file.sha
         with open("C:\\Users\\fede\\Desktop\\dati-bibtex.bib", "r") as file_content:
             file_content_str = file_content.read()
-        # Aggiorna il file con il nuovo contenuto
+            # Aggiorna il file con il nuovo contenuto
             repo.update_file("_bibliography/papers.bib", "update", file_content_str, sha)
 
         print("File file.bib aggiornato su GitHub.")
     except Exception as e:
         print(f"Errore nell'aggiornamento del file: {str(e)}")
 
+
+def startup():
+    CONFIG = "excel_config.json"
+
+    if os.path.isfile(CONFIG) and os.access(CONFIG, os.R_OK) and os.path.exists(CONFIG):
+        with open(CONFIG, 'r') as config_file:
+            dati_utente = json.load(config_file)
+    else:
+        github_token = input("Inserisci il tuo token di GitHub: ")
+        api_key = input("Inserisci la tua Google API key: ")
+        link_bibliografia = input("Inserisci il link alla bibliografia: ")
+        #TODO: aggiungere campi da compilare
+
+        dati_utente = {
+            "_Token": github_token,
+            "_ApiKey": api_key,
+            "BibLink": link_bibliografia
+        }
+
+        with open(CONFIG, "w") as config_file:
+            json.dump(dati_utente, config_file)
+
+    searchForUpdate(dati_utente)
+
+
+def searchForUpdate(dati_utente):
+    # Estrai l'ID del file dal link
+    file_id = dati_utente.get("BibLink").split('/')[-1]
+    api_key = dati_utente.get("_ApiKey")
+
+    # URL per ottenere i metadati del file utilizzando la chiave API
+    metadata_url = f'https://www.googleapis.com/drive/v3/files/{file_id}?fields=name,modifiedTime&key={api_key}'
+
+    try:
+        # Effettua una richiesta GET ai metadati del file
+        response = requests.get(metadata_url)
+
+        if response.status_code == 200:
+            # Estrai l'ora dell'ultima modifica e il nome del file dai metadati
+            data = response.json()
+            ultima_modifica_drive = datetime.fromisoformat(data['modifiedTime'][:-1])
+            nome_file = data['name']
+
+            # Ottieni l'ora dell'ultima modifica del file locale, se esiste
+            percorso_file_locale = os.path.join("edata", nome_file)
+            ultima_modifica_locale = None
+
+            if os.path.exists(percorso_file_locale):
+                ultima_modifica_locale = datetime.utcfromtimestamp(os.path.getmtime(percorso_file_locale))
+
+            # Confronta le date di modifica
+            if ultima_modifica_locale is None or ultima_modifica_drive > ultima_modifica_locale:
+                # Scarica il file solo se il file su Google Drive è stato modificato più recentemente
+                web_file_downloader(file_id, nome_file)
+            else:
+                print(f'Il file locale è già aggiornato.')
+        else:
+            print(f'Errore nella richiesta dei metadati. Codice di stato: {response.status_code}')
+    except Exception as e:
+        print(f'Errore durante la richiesta dei metadati o il download del file: {str(e)}')
+
+
 if __name__ == "__main__":
-    # Utilizza l'URL diretto per il download da Google Drive
     #web_file_downloader("https://drive.google.com/uc?export=download&id=1zPM9n-uXY-oPQWB-oKuz8A5srVEAb6VL", "C:\\Users\\fede\\Desktop\\file-di-prova.xlsx")
     #readexcelfile("C:\\Users\\fede\\Desktop\\file-di-prova.xlsx")
-    gituploader("fedemelis", "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")
+    startup()
+    "https://docs.google.com/spreadsheets/d/17znBoG8Fge2EatZi8j6BFZIP_cbph5CP/edit?usp=drive_link&ouid=105879917516520212135&rtpof=true&sd=true"
+    #gituploader("fedemelis", "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")
