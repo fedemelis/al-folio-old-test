bd74bb5dd3d5a354e04ddce0aa9ec3a881b9e0c7
fedemelis <70884674+fedemelis@users.noreply.github.com>
2023-10-18 20:15:13 +0000

    Deploying to gh-pages from @ fedemelis/fedemelis.github.io@a13fe2badfbd95e10a1ae43ecf6e50cb7a0122b0 🚀

--- excel-processer/thesis.py
+++ excel-processer/thesis.py
@@ -30,7 +30,7 @@ def thesisManipulation(file_name, edatapath):
     custom_order = ["PhD", "Master", "Bachelor"]
 
     # Ordina le tesi in base alla tipologia personalizzata e poi per "anno"
-    sorted_thesis = sorted(data, key=lambda x: (custom_order.index(x["Tipologia"]), x["Anno"]))
+    sorted_thesis = sorted(data, key=lambda x: (custom_order.index(x["Tipologia"]), x["Ciclo/Anno Accademico"]))
 
     # Sovrascrivi il file JSON originale con i dati ordinati
     with open(os.path.join(edatapath, f"{file_name}.json"), "w") as json_data_file:
@@ -50,11 +50,20 @@ def thesisManipulation(file_name, edatapath):
     seenYear = []
     bachelorCount = 0
 
+    markdownText = "---\n" \
+                   "layout: page\n" \
+                   "permalink: /thesis/\n" \
+                   "title: Students\n" \
+                   "description: List of students I have tutored.\n" \
+                   "nav: false\n" \
+                   "nav_order: 5\n" \
+                   "---\n\n"
+
     for entry in data:
         tipologia = entry["Tipologia"]
         nome = entry["Nome"].upper()
         cognome = entry["Cognome"].upper()
-        anno = int(entry["Anno"])
+        anno = entry["Ciclo/Anno Accademico"]
         titolo = entry["Titolo"]
 
         # Controlla il tipo dell'entry (case-insensitive) e aggiorna la lista firstOccurrence
@@ -63,7 +72,7 @@ def thesisManipulation(file_name, edatapath):
             print("Formatto le PhD")
             markdownText += f"## Advisor of {tipologia} student (Doctorate Course in ICT)\n\n"
             firstOccurrence[0] = False
-            markdownText += f"- {nome} {cognome}\n"
+            markdownText += f"- {nome} {cognome} ({anno} Ciclo)\n"
         elif is_type(tipologia, "PhD"):
             markdownText += f"- {nome} {cognome}\n"
         if is_type(tipologia, "Master") and firstOccurrence[1]:
@@ -73,24 +82,24 @@ def thesisManipulation(file_name, edatapath):
             firstOccurrence[1] = False
             seenYear.append(anno)
             markdownText += f"\n{anno}\n"
-            markdownText += f"- {cognome} {nome} {titolo} ({anno})\n"
+            markdownText += f"- {cognome} {nome}: {titolo}, ({anno})\n"
         elif is_type(tipologia, "Master"):
             if anno not in seenYear:
                 seenYear.append(anno)
                 markdownText += f"\n{anno}\n"
-                markdownText += f"- {cognome} {nome} {titolo} ({anno})\n"
+                markdownText += f"- {cognome} {nome}: {titolo}, ({anno})\n"
             else:
-                markdownText += f"- {cognome} {nome} {titolo} ({anno})\n"
+                markdownText += f"- {cognome} {nome}: {titolo}, ({anno})\n"
         if is_type(tipologia, "Bachelor") and firstOccurrence[2]:
             # Primo Bachelor
             print("Formatto le Bachelor")
             markdownText += f"\n## Advisor in {tipologia} Thesis (Dipartimento di Ingegneria \"Enzo Ferrari\")\n\n"
             firstOccurrence[2] = False
             bachelorCount += 1
-            markdownText += f"{bachelorCount}.  {nome} {cognome} {titolo} ({anno})\n"
+            markdownText += f"{bachelorCount}.  {nome} {cognome}: {titolo}, a.a. ({anno})\n"
         elif is_type(tipologia, "Bechelor"):
             bachelorCount += 1
-            markdownText += f"{bachelorCount}.  {nome} {cognome} {titolo} ({anno})\n"
+            markdownText += f"{bachelorCount}.  {nome} {cognome}: {titolo}, a.a. ({anno})\n"
 
     print(markdownText)
 
@@ -98,3 +107,36 @@ def thesisManipulation(file_name, edatapath):
     with open(mdThesis, "w") as markdownTheis:
         markdownTheis.write(markdownText)
         print("Tesi salvate in formato markdown")
+
+
+def gitThesisUploader(dati_utente, edatapath):
+    # Ottieni un'istanza di GitHub usando il token
+    g = Github(dati_utente.get("_Token"))
+
+    repo_name = dati_utente.get("_AccountName") + ".github.io"
+    repo = g.get_user().get_repo(repo_name)
+
+    try:
+        # Prova a ottenere il contenuto del file thesis.md
+        file = repo.get_contents("_pages/thesis.md")
+        sha = file.sha
+        print("File delle tesi esiste su GitHub.")
+    except Exception as e:
+        # Se il file non esiste, crealo
+        if "404" in str(e):
+            with open(os.path.join(edatapath, "thesis.md"), "r") as file_content:
+                file_content_str = file_content.read()
+                repo.create_file("_pages/thesis.md", "Initial thesis file", file_content_str)
+            print("File delle tesi creato su GitHub.")
+
+            with open(os.path.join(edatapath, "thesis.md"), "r") as file_content:
+                file_content_str = file_content.read()
+                repo.update_file("_pages/thesis.md", "automatic thesis update", file_content_str, sha)
+                print("File delle tesi aggiornato su GitHub.")
+                return
+
+    # Se il file esiste, aggiorna il contenuto
+    with open(os.path.join(edatapath, "thesis.md"), "r") as file_content:
+        file_content_str = file_content.read()
+        repo.update_file("_pages/thesis.md", "automatic thesis update", file_content_str, sha)
+        print("File delle tesi aggiornato su GitHub.")
